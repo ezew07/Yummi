8a175bfd400f9b85f19514ac34a0caf9273f069f
William Eze <eze.w@etoncollege.org.uk>
2024-02-23 11:22:09 +0000

    extra stuff

---
+++ Yummi/Views/IndividualRecipie.swift
@@ -0,0 +1,18 @@
+//
+//  IndividualRecipie.swift
+//  Yummi
+//
+//  Created by Eze, William (IRG) on 23/02/2024.
+//
+
+import SwiftUI
+
+struct IndividualRecipie: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IndividualRecipie()
+}
