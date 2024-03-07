3bd8e5726c6b466478576d9af941d2f5ca9c54e7
William Eze <eze.w@etoncollege.org.uk>
2024-03-07 18:23:39 +0000

    partially impleemnted add new recipie. need to allow user input of quantity, unit and category then appennd to recipie object

---
+++ Yummi/ViewModels/CreateRecipieViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CreateRecipieViewModel.swift
+//  Yummi
+//
+//  Created by William Eze on 07/03/2024.
+//
+
+import Foundation
---
+++ Yummi/Views/IngredientsForNewRecipieView.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientsForNewRecipieView.swift
+//  Yummi
+//
+//  Created by William Eze on 07/03/2024.
+//
+
+import SwiftUI
+
+struct IngredientsForNewRecipieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IngredientsForNewRecipieView()
+}
